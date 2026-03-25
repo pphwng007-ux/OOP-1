a5718a3b06d221d15a09b923ada4e886ab6e68c8
phhwng007-ux <pphwng007@gmail.com>
2026-03-25 23:13:54 +0700

    class nhân viên

--- tuan_3/oop_btnc_class.py
+++ tuan_3/oop_btnc_class.py
@@ -1,41 +1,84 @@
 class NhanVien:
-    LUONG_MAX = 20000000
+    LUONG_MAX = 50000000  # lương tối đa
 
     def __init__(self, tenNhanVien, luongCoBan, heSoLuong):
         self.__tenNhanVien = tenNhanVien
         self.__luongCoBan = luongCoBan
         self.__heSoLuong = heSoLuong
 
+    # ===== Getter =====
+    def get_tenNhanVien(self):
+        return self.__tenNhanVien
+
+    def get_luongCoBan(self):
+        return self.__luongCoBan
+
+    def get_heSoLuong(self):
+        return self.__heSoLuong
+
+    def get_LUONG_MAX(self):
+        return NhanVien.LUONG_MAX
+
+    # ===== Setter =====
+    def set_tenNhanVien(self, tenNhanVien):
+        if isinstance(tenNhanVien, str) and tenNhanVien.strip() != "":
+            self.__tenNhanVien = tenNhanVien
+        else:
+            print(" Tên không hợp lệ")
+
+    def set_luongCoBan(self, luongCoBan):
+        if luongCoBan > 0:
+            self.__luongCoBan = luongCoBan
+        else:
+            print(" Lương cơ bản phải > 0")
+
+    def set_heSoLuong(self, heSoLuong):
+        if heSoLuong > 0:
+            self.__heSoLuong = heSoLuong
+        else:
+            print(" Hệ số lương phải > 0")
+
+    def set_LUONG_MAX(self, luong_max):
+        if luong_max > 0:
+            NhanVien.LUONG_MAX = luong_max
+        else:
+            print(" LUONG_MAX phải > 0")
+
+    # ===== Tính lương =====
     def tinhLuong(self):
         return self.__luongCoBan * self.__heSoLuong
 
-    def inTTin(self):
-        print("Tên:", self.__tenNhanVien)
-        print("Lương:", self.tinhLuong())
-
+    # ===== Tăng lương (boolean) =====
     def tangLuong(self, delta):
-        heSoMoi = self.__heSoLuong + delta
-        luongMoi = self.__luongCoBan * heSoMoi
+        luong_moi = (self.__luongCoBan + delta) * self.__heSoLuong
 
-        if luongMoi > NhanVien.LUONG_MAX:
-            print("Vượt max!")
+        if luong_moi > NhanVien.LUONG_MAX:
+            print(" Lương vượt quá mức tối đa!")
             return False
         else:
-            self.__heSoLuong = heSoMoi
+            self.__luongCoBan += delta
             return True
 
+    # ===== In thông tin =====
+    def inTTin(self):
+        print("Tên:", self.__tenNhanVien)
+        print("Lương cơ bản:", self.__luongCoBan)
+        print("Hệ số lương:", self.__heSoLuong)
+        print("=> Lương:", self.tinhLuong())
 
-# 🔻 Nhập dữ liệu
-ten = input("Nhập tên: ")
-luongCoBan = float(input("Nhập lương cơ bản: "))
-heSoLuong = float(input("Nhập hệ số lương: "))
 
-nv = NhanVien(ten, luongCoBan, heSoLuong)
+# ===== TEST =====
+if __name__ == "__main__":
+    nv = NhanVien("Phuong", 5000000, 3)
 
-nv.inTTin()
+    print("=== Thông tin ban đầu ===")
+    nv.inTTin()
 
-# test tăng lương
-delta = float(input("Nhập mức tăng: "))
-nv.tangLuong(delta)
+    print("\n=== Tăng lương ===")
+    if nv.tangLuong(2000000):
+        print(" Tăng lương thành công")
+    else:
+        print(" Tăng lương thất bại")
 
-nv.inTTin()
+    print("\n=== Sau khi tăng ===")
+    nv.inTTin()
